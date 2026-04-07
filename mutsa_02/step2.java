f6aab13bc756587d4908051d385cfb5376c1b850
JHPARK <jhpark613@naver.com>
2026-04-07 15:09:03 +0900

    FIX

--- mutsa_02/step2.java
+++ mutsa_02/step2.java
@@ -4,14 +4,14 @@ import java.util.Scanner;
 
 public class step2 {
     static void main(String[] args) {
-        System.out.println("아기사자 이름을 입력해주세요.");
+        System.out.println("\uD83E\uDD81 아기사자 이름을 입력해주세요.");
         Scanner sc = new Scanner(System.in);
         String name = sc.nextLine();
 
-        System.out.println("전공을 입력해주세요.");
+        System.out.println("\uD83C\uDF93 전공을 입력해주세요.");
         String major = sc.nextLine();
 
-        System.out.println("기수를 입력해주세요.");
+        System.out.println("\uD83D\uDCCC 기수를 입력해주세요.");
         int number = sc.nextInt();
 
         Lion lion = new Lion(name, major,number); //lion 객체 생성, 매개변수에 값 초기화를 위해
--- mutsa_02_2/step3.java
+++ mutsa_02_2/step3.java
@@ -6,14 +6,14 @@ import java.util.Scanner;
 
 public class step3 {
     public static void main(String[] args) {
-        System.out.println("아기사자 이름을 입력해주세요.");
+        System.out.println("\uD83E\uDD81 아기사자 이름을 입력해주세요.");
         Scanner sc = new Scanner(System.in);
         String name = sc.nextLine();
 
-        System.out.println("전공을 입력해주세요.");
+        System.out.println("\uD83C\uDF93 전공을 입력해주세요.");
         String major = sc.nextLine();
 
-        System.out.println("기수를 입력해주세요.");
+        System.out.println("\uD83D\uDCCC 기수를 입력해주세요.");
         int number = sc.nextInt();
 
         Lion lion = new Lion(name,major,number);
